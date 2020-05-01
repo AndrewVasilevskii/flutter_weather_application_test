24aed3e9b35ba4a71285aa114d34ee555a8bc45e
Andrew <andrew.vasilevskii@gmail.com>
2020-05-01 20:18:34 +0300

    BLoC implementation
    - created Events for bloc
    - created States for bloc
    - created simple bloc
    - created bloc delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc_delegate.dart';
 import 'package:flutterweatherapplication/api_clients/location_api_client.dart';
 import 'package:flutterweatherapplication/api_clients/weather_api_client.dart';
 import 'package:flutterweatherapplication/repositories/weather_repository.dart';
 
 void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   final WeatherRepository weatherRepository = WeatherRepository(
     locationApiClient: LocationApiClient(),
     weatherApiClient: WeatherApiClient(httpClient: http.Client())
